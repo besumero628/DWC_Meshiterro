5088456f393bd314c7fc804b7d082e9706e40ac0
besumero628 <besumero628@gmail.com>
2021-07-04 15:04:53 +0000

    [Update] index view & refile key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b369ca007f7a1d2eace891ef94a4a8cdb6d11a6a0dddb31fc8cd274672141a72a85683398b3b3935be9a8d98ac68ca2e88fa30352859f92a0e56ddee8225ba08'
